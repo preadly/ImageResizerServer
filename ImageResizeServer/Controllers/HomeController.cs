96ac590277a4daf8d0a4f9759a25374df618d574
rbnunes <rn@pread.ly>
2015-03-10 21:35:33 -0300

    Initial Commit
    Eveyrthing is ready is just a clever user of ImageResizing.net

---
+++ ImageResizeServer/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageResizeServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectPermanent("http://pread.ly");
+        }
+
+        public ActionResult HeartBeat()
+        {
+            return Content("OK");
+        }
+
+    }
+}
